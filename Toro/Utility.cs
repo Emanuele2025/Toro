dd0e1323ad1cbbcac77d63d29b526181be22a266
Emanuele2025 <emanuelemattei@tutanota.com>
2026-06-26 08:46:50 +0200

    Aggiunta classe per le funzioni da utilizzare comunemente

---
+++ Toro/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toro
+{
+    /// <summary>
+    /// Classe con varie funzionalità da utilizzare nel progetto
+    /// </summary>
+    public static class Utility
+    {
+
+        #region Messaggio a video
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
