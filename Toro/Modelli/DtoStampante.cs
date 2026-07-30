68c1e7eef2e465d69b64b1b420434ffc2b9b4327
Emanuele2025 <emanuelemattei@tutanota.com>
2026-07-30 19:12:45 +0200

    classe gestione valori stampante

--- Toro/Modelli/DtoStampante.cs
+++ Toro/Modelli/DtoStampante.cs
@@ -6,5 +6,15 @@ namespace Toro.Modelli
 {
     public class DtoStampante
     {
+        public string Nome { get; set; }
+        public string NomeDriver { get; set; }
+        public string Porta { get; set; }
+        public bool Rete { get; set; }
+        public bool Predefinita { get; set; }
+        public string Stato { get; set; }
+        public string Modello { get; set; }
+        public string Descrizione { get; set; }
+        public bool Online { get; set; }
+        public int NumeroLavoro { get; set; }
     }
 }
