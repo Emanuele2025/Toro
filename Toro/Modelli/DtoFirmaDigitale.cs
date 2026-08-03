0a46e634e728140c0ea0ed0ec06ecc858bdbdc2c
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-03 06:30:30 +0200

    gestione firma digitale

---
+++ Toro/Modelli/DtoFirmaDigitale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toro.Modelli
+{
+    /// <summary>
+    /// Per la gestione dei file firmati digitalmente
+    /// </summary>
+    public class DtoFirmaDigitale
+    {
+        public string NomeFirmatario { get; set; }
+
+        public string Oggetto { get; set; }
+
+        public DateTime DataInizio { get; set; }
+
+        public DateTime DataFine { get; set; }
+
+    }
+}
