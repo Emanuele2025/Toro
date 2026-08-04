d4cd4584228729c4008d63baf988db355757a60b
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-04 06:17:43 +0200

    modifiche per scheda

---
+++ Toro/Modelli/DtoSchedaRete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toro.Modelli
+{
+    public class DtoSchedaRete
+    {
+        public string Nome { get; set; }
+
+        public string Modello { get; set; }
+
+        public string Driver { get; set; }
+
+
+
+
+
+    }
+}
