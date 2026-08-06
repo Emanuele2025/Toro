ef5bf1ba3c325dce1218c1713258173683b6e9cd
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-06 07:48:06 +0200

    gestione file

--- Toro/FrmFile.Designer.cs
+++ Toro/FrmFile.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            tbcFile = new TabControl();
+            tbpLeggiCertificatiPdf = new TabPage();
+            tabPage2 = new TabPage();
+            BtnChiudi = new Button();
+            dtgCertificatiFilePdf = new DataGridView();
+            label2 = new Label();
+            BtnCercaCartella = new Button();
+            TxtPercorsoCartella = new TextBox();
+            BtnLeggiCertificatiPdf = new Button();
+            tbcFile.SuspendLayout();
+            tbpLeggiCertificatiPdf.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dtgCertificatiFilePdf).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.Highlight;
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(963, 22);
+            label1.TabIndex = 2;
+            label1.Text = "Toro- Gestione dei file ";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // tbcFile
+            // 
+            tbcFile.Controls.Add(tbpLeggiCertificatiPdf);
+            tbcFile.Controls.Add(tabPage2);
+            tbcFile.Dock = DockStyle.Top;
+            tbcFile.Location = new Point(0, 22);
+            tbcFile.Name = "tbcFile";
+            tbcFile.SelectedIndex = 0;
+            tbcFile.Size = new Size(963, 284);
+            tbcFile.TabIndex = 3;
+            // 
+            // tbpLeggiCertificatiPdf
+            // 
+            tbpLeggiCertificatiPdf.Controls.Add(BtnLeggiCertificatiPdf);
+            tbpLeggiCertificatiPdf.Controls.Add(BtnCercaCartella);
+            tbpLeggiCertificatiPdf.Controls.Add(TxtPercorsoCartella);
+            tbpLeggiCertificatiPdf.Controls.Add(label2);
+            tbpLeggiCertificatiPdf.Controls.Add(dtgCertificatiFilePdf);
+            tbpLeggiCertificatiPdf.Location = new Point(4, 24);
+            tbpLeggiCertificatiPdf.Name = "tbpLeggiCertificatiPdf";
+            tbpLeggiCertificatiPdf.Padding = new Padding(3);
+            tbpLeggiCertificatiPdf.Size = new Size(955, 256);
+            tbpLeggiCertificatiPdf.TabIndex = 0;
+            tbpLeggiCertificatiPdf.Text = "Certificati file PDF";
+            tbpLeggiCertificatiPdf.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(192, 72);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // BtnChiudi
+            // 
+            BtnChiudi.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            BtnChiudi.Image = Properties.Resources.chiudi_16;
+            BtnChiudi.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnChiudi.Location = new Point(841, 327);
+            BtnChiudi.Name = "BtnChiudi";
+            BtnChiudi.Size = new Size(75, 23);
+            BtnChiudi.TabIndex = 4;
+            BtnChiudi.Text = "Chiudi";
+            BtnChiudi.UseVisualStyleBackColor = true;
+            // 
+            // dtgCertificatiFilePdf
+            // 
+            dtgCertificatiFilePdf.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgCertificatiFilePdf.Dock = DockStyle.Bottom;
+            dtgCertificatiFilePdf.Location = new Point(3, 76);
+            dtgCertificatiFilePdf.Name = "dtgCertificatiFilePdf";
+            dtgCertificatiFilePdf.Size = new Size(949, 177);
+            dtgCertificatiFilePdf.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 4);
+            label2.Name = "label2";
+            label2.Size = new Size(324, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Percorso e nome del file Pdf da estrapolare certificati file pdf";
+            // 
+            // BtnCercaCartella
+            // 
+            BtnCercaCartella.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            BtnCercaCartella.Location = new Point(572, 19);
+            BtnCercaCartella.Name = "BtnCercaCartella";
+            BtnCercaCartella.Size = new Size(30, 29);
+            BtnCercaCartella.TabIndex = 15;
+            BtnCercaCartella.Text = "...";
+            BtnCercaCartella.TextAlign = ContentAlignment.TopLeft;
+            BtnCercaCartella.UseVisualStyleBackColor = true;
+            BtnCercaCartella.Click += BtnCercaCartella_Click;
+            // 
+            // TxtPercorsoCartella
+            // 
+            TxtPercorsoCartella.Location = new Point(17, 22);
+            TxtPercorsoCartella.Name = "TxtPercorsoCartella";
+            TxtPercorsoCartella.ReadOnly = true;
+            TxtPercorsoCartella.Size = new Size(555, 23);
+            TxtPercorsoCartella.TabIndex = 14;
+            // 
+            // BtnLeggiCertificatiPdf
+            // 
+            BtnLeggiCertificatiPdf.Location = new Point(17, 48);
+            BtnLeggiCertificatiPdf.Name = "BtnLeggiCertificatiPdf";
+            BtnLeggiCertificatiPdf.Size = new Size(92, 23);
+            BtnLeggiCertificatiPdf.TabIndex = 16;
+            BtnLeggiCertificatiPdf.Text = "Rileva Certificati";
+            BtnLeggiCertificatiPdf.UseVisualStyleBackColor = true;
             // 
             // FrmFile
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(963, 362);
+            Controls.Add(BtnChiudi);
+            Controls.Add(tbcFile);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
@@ -42,9 +166,24 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Toro";
             Load += FrmFile_Load;
+            tbcFile.ResumeLayout(false);
+            tbpLeggiCertificatiPdf.ResumeLayout(false);
+            tbpLeggiCertificatiPdf.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dtgCertificatiFilePdf).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label label1;
+        private TabControl tbcFile;
+        private TabPage tbpLeggiCertificatiPdf;
+        private TabPage tabPage2;
+        private Button BtnChiudi;
+        private Button BtnLeggiCertificatiPdf;
+        private Button BtnCercaCartella;
+        private TextBox TxtPercorsoCartella;
+        private Label label2;
+        private DataGridView dtgCertificatiFilePdf;
     }
 }
--- Toro/FrmFile.cs
+++ Toro/FrmFile.cs
@@ -36,5 +36,10 @@ namespace Toro
 
 
         }
+
+        private void BtnCercaCartella_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
