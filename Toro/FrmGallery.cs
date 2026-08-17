c6f533876f958f83e041a97bd47d98302860d323
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-17 06:41:40 +0200

    finestra galleria fotografica

---
+++ Toro/FrmGallery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Toro
+{
+    public partial class FrmGallery : Form
+    {
+        public FrmGallery()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGallery_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
