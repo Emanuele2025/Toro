4be39b8d39bb6f837d7290dbfb3cb3fd01568fdb
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-21 15:18:23 +0200

    gestione download file bing

--- Toro/FrmImmaginiMS.cs
+++ Toro/FrmImmaginiMS.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.Text.Json;
 using System.Windows.Forms;
 
 namespace Toro
@@ -62,6 +63,49 @@ namespace Toro
 
         }
 
+        static async Task DownloadImmagine(string percorsoSalvataggio)
+        {
+            //Percorso di Bing dove estrapolare l'immagine
+            string bingApiUrl = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=it-IT";
+
+            //Richiesta http
+            using HttpClient client = new HttpClient();
+
+            try
+            {
+                //Rilevo codice html per la gestione JSON
+                string json = await client.GetStringAsync(bingApiUrl);
+
+                //Estraggo il nome dell'immagine ed il percorso dell'immagine bing
+                using JsonDocument doc = JsonDocument.Parse(json);
+                string urlBase = doc.RootElement
+                    .GetProperty("images")[0]
+                    .GetProperty("urlbase")
+                    .GetString();
+
+                //Nome del file da salvare
+                string nomeFile = urlBase?.Split(".").Length > 1 ? urlBase.Split(".")[1] : urlBase;
+                //rileva il sito preciso con l'immagine
+                string imageUrl = $"https://www.bing.com{urlBase}_1920x1080.webp";
+                nomeFile = percorsoSalvataggio + "\\" + nomeFile + ".webp";
+
+                //Scarico l'immagine in array di byte
+                byte[] imageBytes = await client.GetByteArrayAsync(imageUrl);
+                await File.WriteAllBytesAsync(nomeFile, imageBytes);
+
+
+                Utility.MessaggioInfo("File salvato con successo.");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Utility.MessaggioErrore(ex.Message);
+            }
+        }
+
+
 
 
         #endregion
@@ -77,15 +121,55 @@ namespace Toro
 
 
 
-        private void BtnScaricaSfondoBing_Click(object sender, EventArgs e)
+        private async void BtnScaricaSfondoBing_Click(object sender, EventArgs e)
         {
+            Cursor = Cursors.WaitCursor;
             try
             {
+                if (TxtPercorsoCartella.Text.Trim() == "")
+                {
+
+                    Utility.MessaggioInfo("Impostare un percorso dove salvare l'immagine.");
+                    return;
+                }
+                
+
+
+                await DownloadImmagine(TxtPercorsoCartella.Text.Trim());
+                
+                var files = new DirectoryInfo(TxtPercorsoCartella.Text.Trim()).GetFiles()
+                    .Select(file => new
+                    {
+                        NomeFile = file.Name,
+                        DataCreazione = file.CreationTime
+                    })
+                    .Where(file => file.NomeFile.EndsWith(".webp", StringComparison.OrdinalIgnoreCase))
+                    .OrderByDescending(file => file.DataCreazione)
+                    .ToList();
+
+                List<string> fileNames = files.Select(f => f.NomeFile).ToList();
+               // lsbListaFile.DataSource = fileNames;
+
+                // Se ci sono file, seleziona il primo e carica l'immagine
+                if (fileNames.Count > 0)
+                {
+                    //lsbListaFile.SelectedIndex = 0;
+                    //CaricaImmagineDaListBox();
+                }
+                
+
+                
+
+
 
             }
             catch (Exception ex)
             {
                 Utility.MessaggioErrore(ex.Message);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
             }
         }
 
